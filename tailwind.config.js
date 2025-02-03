b85ec557a65fc65728e283a22e5a1ea35db972ff
wickborlan <wickborlan@gmail.com>
2025-02-03 22:32:27 +0700

    create - page login

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './index.html',
+        './dist/*.{html,js}',
+        './dist/page/*.{html,js}'
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
